35ae819b60a6d921f39cbb04a17e5cfdb582fc7e
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-10-18 09:29:23 +0200

    Finished the logout functionality

--- elearning_system/users/urls.py
+++ elearning_system/users/urls.py
@@ -6,8 +6,8 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/<int:user_id>/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='users:login'), name='logout'),
 ]
